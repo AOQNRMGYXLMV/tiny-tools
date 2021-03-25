34fa2a56b020113859d805704842ed03591ff194
Yuhan Si <489365227@qq.com>
2021-03-25 20:59:25 +0800

    add loading indicator

--- components/fetch_cover.js
+++ components/fetch_cover.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Button, Input } from 'antd'
+import { Button, Input, message } from 'antd'
 import Image from 'next/image'
 
 export default function FetchCover() {
@@ -7,10 +7,20 @@ export default function FetchCover() {
   const [url, setUrl] = useState('')
 
   const onSearch = value => {
+    if (value.length === 0) {
+      return
+    }
+    const legal = value.startsWith('https://mp.weixin.qq.com')
+    if (!legal) {
+      message.error('您输入的链接地址不合法，请重新输入')
+      return
+    }
+
+    setLoading(true);
     fetch(`/api/fetch_cover?article=${value}`).then(res => res.json()).then(data => {
       return new Promise(resolve => {
-        console.log('data: ', data)
         setUrl(data.url)
+        setLoading(false)
         resolve()
       })
     })
@@ -23,6 +33,7 @@ export default function FetchCover() {
         allowClear
         enterButton="提取封面"
         size="large"
+        loading={loading}
         onSearch={onSearch}
       />
       {url && <Button type="primary"><a href={url} target="_blank">点击打开封面</a></Button>}
